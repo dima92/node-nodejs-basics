5943ff1936de23bc13e2ebdda5152ad63c6040b2
dima92 <dima-tkachenko@mail.ru>
2022-05-30 21:47:46 +0300

    feat: implement task - create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,25 @@
+import {fileURLToPath} from 'url'
+import {dirname} from 'path'
+import {writeFile, access} from 'fs/promises'
+import {constants} from 'fs'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 export const create = async () => {
-    // Write your code here 
+
+  try {
+    const fileToPath = __dirname + '\\' + 'files' + '\\' + 'fresh.txt'
+    await writeFile(fileToPath, 'I am fresh and young', (err) => {
+      err ? console.log(err) : null
+    })
+    await access(fileToPath, constants.F_OK, (err) => {
+      if (err)
+        console.log(err)
+    })
+  } catch (e) {
+    throw new Error('FS operation failed')
+  }
 };
+
+create()

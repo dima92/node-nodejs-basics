734c88726242d44899fc88a51a2da30f018acf0c
dima92 <dima-tkachenko@mail.ru>
2022-06-02 15:28:47 +0300

    feat: implement task - delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,24 @@
+import {fileURLToPath} from 'url'
+import {dirname} from 'path'
+import * as fs from 'fs'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const pathToFile = __dirname + '\\' + 'files' + '\\' + 'fileToRemove.txt'
+
 export const remove = async () => {
-    // Write your code here 
+  fs.access(pathToFile, fs.constants.R_OK | fs.constants.W_OK, (err) => {
+    console.log('checking access')
+    if (err) {
+      throw new Error('FS operation failed')
+    } else {
+      console.log('File can be read')
+    }
+  })
+  fs.unlink(pathToFile, () => {
+    console.log(`Remove file ${pathToFile}`)
+  })
 };
+
+remove()

69199501175cafea7e76071204e0fab9e1d4e368
dima92 <dima-tkachenko@mail.ru>
2022-06-02 14:56:05 +0300

    feat: implement task - copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,28 @@
+import {fileURLToPath} from 'url'
+import {dirname} from 'path'
+import {access, copyFile} from 'fs/promises'
+import {constants, mkdir, readdir} from 'fs'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const source = __dirname + '\\' + 'files'
+const destination = __dirname + '\\' + 'files_copy'
+
 export const copy = async () => {
-    // Write your code here 
-};
+  // if (await access(destination, constants.R_OK | constants.W_OK)) {
+  await mkdir(destination, {recursive: true}, (err) => {
+    err ? console.log(err) : null
+    readdir(source, {withFileTypes: true}, (err, files) => {
+      err ? console.log(err) : null
+      for (let file of files) {
+        const filename = file.name.toString()
+
+        copyFile(source + '\\' + filename, destination + '\\' + filename)
+          .catch(err => console.log(err))
+      }
+    })
+  })
+}
+
+copy()

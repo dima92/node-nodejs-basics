82d02e6f4ddb2f9e0bdb4b5ce4138b6fda07fab8
dima92 <dima-tkachenko@mail.ru>
2022-06-02 15:17:43 +0300

    feat: implement task - rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,21 @@
+import {fileURLToPath} from 'url'
+import {dirname} from 'path'
+import * as fs from 'fs'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const pathToFile = __dirname + '\\' + 'files' + '\\' + 'wrongFilename.txt'
+const pathToFileRename = __dirname + '\\' + 'files' + '\\' + 'properFilename.md'
+
 export const rename = async () => {
-    // Write your code here 
+  try {
+    fs.rename(pathToFile, pathToFileRename, () => {
+      console.log('File renamed.')
+    })
+  } catch (e) {
+    throw new Error(e)
+  }
 };
+
+rename()

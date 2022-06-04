a719eb1a785f1404a41b4f91a2876efb33cd2677
dima92 <dima-tkachenko@mail.ru>
2022-06-04 10:14:30 +0300

    feat: implement task - write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,17 @@
+import {fileURLToPath} from 'url'
+import {dirname} from 'path'
+import * as fs from 'fs'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const pathToFile = __dirname + '\\' + 'files' + '\\' + 'fileToWrite.txt'
+
 export const write = async () => {
-    // Write your code here 
+  const writeStream = fs.createWriteStream(pathToFile)
+  process.stdin.on('data', data => {
+    writeStream.write(data.toString())
+  })
 };
+
+write()

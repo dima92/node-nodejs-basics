8bb71bc9b76e89123d72c94856093a1254b14633
dima92 <dima-tkachenko@mail.ru>
2022-06-05 07:02:54 +0300

    feat: implement task - worker.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,21 @@
+import * as workerJS from 'worker_threads'
+import * as os from 'os'
+
 export const performCalculations = async () => {
-    // Write your code here
+  const cpus = os.cpus()
+  const arr = []
+  for (let i = 1; i <= cpus.length; i++) {
+    arr.push(new Promise((resolve, reject) => {
+      resolve(new workerJS.Worker('./worker.js', {
+        workerData: 10 + i
+      }).on('message', msg => {
+        return msg
+      }))
+    }))
+  }
+  await Promise.allSettled(arr).then(value => {
+    console.log(value)
+  })
 };
+
+performCalculations()

67d26bca60183e5757f32ae12c92aee0e0b73dd5
dima92 <dima-tkachenko@mail.ru>
2022-06-05 06:21:16 +0300

    feat: implement task - transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -3,7 +3,7 @@ import {Transform, pipeline} from 'stream'
 const readable = process.stdin
 const writable = process.stdout
 
-const transform = new Transform({
+export async const transform = new Transform({
   transform(chunk, encoding, callback) {
     const chunkStringified = chunk.toString().trim()
     const reversedChunck = chunkStringified.split('').reverse().join('')

cf311c4e44042798b380edee175e11888fd6aa10
dima92 <dima-tkachenko@mail.ru>
2022-06-05 06:42:56 +0300

    feat: implement task - cjsToEsm.mjs

---
+++ src/modules/cjsToEsm.mjs
@@ -0,0 +1,37 @@
+import * as path from 'path'
+import {release, version} from 'os';
+import {createServer} from 'http';
+import {fileURLToPath} from "url";
+import {dirname} from "path";
+
+import('./files/c.js');
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const random = Math.random();
+
+let unknownObject;
+
+if (random > 0.5) {
+  unknownObject = await import('./files/a.json', {assert: {type: "json"}});
+} else {
+  unknownObject = await import('./files/b.json', {assert: {type: "json"}});
+}
+
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${path.sep}"`);
+
+console.log(`Path to current file is ${__filename}`);
+console.log(`Path to current directory is ${__dirname}`);
+
+const createMyServer = createServer((_, res) => {
+  res.end('Request accepted');
+});
+
+export default {
+  unknownObject,
+  createMyServer,
+};
+
